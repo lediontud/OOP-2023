f6268e046000fbf4a9c88addb35e32d70917f4e8
lediontud <c21317311@mytudublin.ie>
2023-02-13 11:35:23 +0000

    practice

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,35 +1,24 @@
 package ie.tudublin;
 
-public class Main
-{
+public class Main {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
+	public static void helloProcessing() {
+		String[] a = { "MAIN" };
+		processing.core.PApplet.runSketch(a, new HelloProcessing());
+	}
 
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
+	public static void loops() {
+		String[] a = { "MAIN" };
+		processing.core.PApplet.runSketch(a, new Loops());
+	}
 
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
+	public static void bugZap() {
+		String[] a = { "MAIN" };
+		processing.core.PApplet.runSketch(a, new BugZap());
+	}
 
+	public static void main(String[] args) {
 		loops();
 	}
-	
+
 }
