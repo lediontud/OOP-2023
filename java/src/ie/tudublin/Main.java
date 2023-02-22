fad5d0b22caa0e9beb54dee74ec5936fdea3a6e6
lediontud <c21317311@mytudublin.ie>
2023-02-22 10:26:15 +0000

    lab4

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -21,7 +21,7 @@ public class Main {
 	public static void starMap()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
+        //processing.core.PApplet.runSketch( a, new StarMap());
     }
 	
 	public static void main(String[] args)
